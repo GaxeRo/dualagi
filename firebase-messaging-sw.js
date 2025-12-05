e677bf843ed30a11f7e1b32da1c22f088b8de518
Arfiansyah <arfiansyah2702@gmail.com>
2025-12-05 09:10:27 +0700

    Fix Firebase initialization and background message handling
    Updated Firebase messaging service worker to correctly initialize Firebase and handle background messages.

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,7 +2,6 @@ importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  const firebaseConfig = {
   apiKey: "AIzaSyAWVllyvpOvS5mDs4IVnHtZXuTvDwGT0Z8",
   authDomain: "habibah-a1416.firebaseapp.com",
   projectId: "habibah-a1416",
@@ -10,22 +9,16 @@ firebase.initializeApp({
   messagingSenderId: "166211722150",
   appId: "1:166211722150:web:229159150cb95dbd140d67",
   measurementId: "G-ZXTWQ13DCZ"
-};
+});
 
 const messaging = firebase.messaging();
 
-// Listener notifikasi ketika web ditutup / background
+// menerima notifikasi ketika web ditutup
 messaging.onBackgroundMessage(function (payload) {
-  console.log("Background message received:", payload);
+  console.log("Background message:", payload);
 
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
+  self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
     icon: "icon.png"
-  };
-
-  self.registration.showNotification(
-    notificationTitle,
-    notificationOptions
-  );
+  });
 });
